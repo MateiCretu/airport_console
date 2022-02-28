e327b52260f8c28783288b94db910fc787f6ddc6
MateiCretu <mateicretu@yahoo.com>
2022-02-28 13:13:39 +0200

    test_commit

--- src/Application.java
+++ src/Application.java
@@ -1,11 +1,16 @@
-import java.time.LocalTime;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Application {
     public static void main(String[] args) {
+        Set<Integer> lockers = new TreeSet<>();
+        for (int n = 0, m = 1; n < 100; n++, m++) {
+            lockers.add(m);
+        }
+        System.out.println("Hello!" +
+                "\nIn order to select a locker please enter 1." +
+                "\nIn order to free up a locker enter 2." +
+                "\nTo exit press 0!");
 
-        int id = 10;
-        int pin = 1010;
-        Locker locker = new Locker(id, pin, LocalTime.now());
-        System.out.println(locker);
     }
 }
