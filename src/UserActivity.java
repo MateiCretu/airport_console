8010c3ac44c49c9fe239fec5e8eaa41d5e01d0ed
MateiCretu <mateicretu@yahoo.com>
2022-02-28 20:32:37 +0200

    set up payment method start to build app

--- src/UserActivity.java
+++ src/UserActivity.java
@@ -3,20 +3,15 @@ public class UserActivity {
 
     private long payment(long timeSpent) {
         int sumToPay = 0;
-        if (timeSpent <= 60) {
-            sumToPay = 10;
-            System.out.println("You must pay: " + sumToPay + " RON.");
-        }
-        else {
             sumToPay = (int) (10 + timeSpent / 60 * 5);
             System.out.println("You must pay: " + sumToPay + " RON.");
-        }
+
         return sumToPay;
     }
 
     public static void main(String[] args) {
         UserActivity user = new UserActivity();
-        user.payment(270);
+        user.payment(59);
     }
 
 }
